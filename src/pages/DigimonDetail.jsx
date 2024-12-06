81dcb1d89810918e6843312378394ee1dc02e174
GwenZi0u0 <a0918566336@gmail.com>
2024-12-07 00:38:34 +0800

    Updates

--- src/pages/DigimonDetail.jsx
+++
@@ -1,7 +0,0 @@
-export const DigimonDetail = () => {
-  return (
-    <div className="flex flex-col items-center justify-center w-full h-full space-y-4">
-      <h1 className="text-4xl font-bold">Welcome to the DigimonDetail </h1>
-    </div>
-  );
-};
